4cc4c1a94be285f16ee77b639ed8262c62e44a2e
Alexnander Nasokin <alexnas2python@gmail.com>
2023-03-13 13:23:31 +0300

    Create Note Form

---
+++ components/notes/Form.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+const notesInput = reactive({
+  title: '',
+  note: '',
+});
+</script>
+
+<template>
+  <div>
+    <NCard class="card">
+      <input class="input" type="text" placeholder="My notes title" v-model="notesInput.title" />
+      <textarea class="input" placeholder="My notes" v-model="notesInput.note" />
+      <NButton>Save Note</NButton>
+    </NCard>
+  </div>
+</template>
+
+<style scoped>
+.card {
+  padding: 1.5rem;
+  display: flex;
+  flex-direction: column;
+}
+.input {
+  border: 0.1rem solid rgba(0, 0, 0, 0.2);
+  margin-bottom: 0.5em;
+  padding: 0.2rem;
+  border-radius: 0.25rem;
+}
+</style>
