8656b3b85f9ab0b3c085ca020218435127d77f79
Alexnander Nasokin <alexnas2python@gmail.com>
2023-03-14 14:01:53 +0300

    Fetch Notes Data from Supabase Database'

--- pages/profile.vue
+++ pages/profile.vue
@@ -1,4 +1,15 @@
 <script setup lang="ts">
+const { supabase } = useSupabase();
+const { user } = useAuth();
+
+const notesResponse = ref();
+
+if (process.client) {
+  notesResponse.value = await supabase.from('notes').select().eq('user_id', user.value?.id);
+}
+
+console.log('user', user.value?.id);
+
 definePageMeta({
   middleware: 'auth',
 });
@@ -9,6 +20,13 @@ definePageMeta({
     <div class="container">
       <h3>My Notes</h3>
       <NotesForm />
+      notesResponse {{ notesResponse }}
+      <div v-if="notesResponse.data" class="card-container">
+        <NCard class="card" v-for="note in notesResponse.data" key="note.id">
+          <h2>{{ note.title }}</h2>
+          <p>{{ note.note }}</p>
+        </NCard>
+      </div>
     </div>
   </div>
 </template>
@@ -22,5 +40,16 @@ definePageMeta({
 }
 h3 {
   font-size: 2rem;
+}
+.card {
+  padding: 1rem;
+  margin-top: 1.5rem;
+}
+h2 {
+  font-weight: bold;
+  font-size: 1.5rem;
+}
+.card-container {
+  margin-top: 1.5rem;
 }
 </style>
